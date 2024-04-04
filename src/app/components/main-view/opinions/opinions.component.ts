d0a106d661bcb4f14dea516676af6272361191a3
Cianuro <pikabz2638@gmail.com>
2024-04-04 10:34:20 +0200

    opinions done

--- src/app/components/main-view/opinions/opinions.component.ts
+++ src/app/components/main-view/opinions/opinions.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NgOptimizedImage } from '@angular/common';
+
+export interface Opinion {
+  name: string;
+  opinion: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-opinions',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './opinions.component.html',
-  styleUrl: './opinions.component.scss'
+  styleUrl: './opinions.component.scss',
 })
 export class OpinionsComponent {
-
+  opinions: Opinion[] = [
+    {
+      name: 'Emma Thompson',
+      opinion: `TinyTask simplifies my day with its seamless integration of to-do lists and calendars. It's my go-to for staying on top of my workload.`,
+      img: 'assets/imgs/people1.jpg',
+    },
+    {
+      name: 'Clara Johnson',
+      opinion: `TinyTask's customizable categories help me stay organized across projects, boosting my productivity as a freelancer.`,
+      img: 'assets/imgs/people2.jpg',
+    },
+    {
+      name: 'Maxwell Parker',
+      opinion: `TinyTask keeps me organized anywhere with its reminders and notifications. It's now essential to my daily routine.`,
+      img: 'assets/imgs/people1.jpg',
+    },
+    {
+      name: 'Jackson Smith',
+      opinion: `TinyTask's intuitive design helps me stay focused on tasks, whether work or personal. Highly recommended for schedule control.`,
+      img: 'assets/imgs/people3.jpg',
+    },
+  ];
 }
