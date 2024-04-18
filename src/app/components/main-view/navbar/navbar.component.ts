ab45a9eb38f3c8f9da93e7d6a9047d9ad1e48acf
Cianuro <pikabz2638@gmail.com>
2024-04-18 11:07:11 +0200

    sidenav for users

--- src/app/components/main-view/navbar/navbar.component.ts
+++ src/app/components/main-view/navbar/navbar.component.ts
@@ -1,14 +1,27 @@
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { NgIf, NgOptimizedImage } from '@angular/common';
 import {
   Router,
   RouterLink,
   RouterLinkActive,
   RouterModule,
 } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faAddressBook,
+  faBell,
+  faCircleUser,
+  faGear,
+  faListCheck,
+  faTags,
+  faUserLock,
+} from '@fortawesome/free-solid-svg-icons';
 import { StorageService } from '../../../services/storage/storage.service';
+
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-navbar',
@@ -21,13 +34,27 @@ import { StorageService } from '../../../services/storage/storage.service';
     RouterLink,
     RouterLinkActive,
     NgIf,
+    FontAwesomeModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
+  // Icons
+  faUser = faCircleUser;
+  faTodo = faListCheck;
+  faTags = faTags;
+  faFriends = faAddressBook;
+  faNoti = faBell;
+  faconfig = faGear;
+  faAdmin = faUserLock;
+  // Variables
   isMenuOpen = false;
   isLoggedIn = false;
+  isAdmin = true;
+  switchProfile = false;
 
   constructor(private storageService: StorageService, private router: Router) {}
 
@@ -45,5 +72,10 @@ export class NavbarComponent implements OnInit {
     this.router.navigate(['/login']).then(() => {
       console.log('Logout OK, cargando login...');
     });
+  }
+
+  changeProfile() {
+    this.switchProfile = true;
+    console.log('ola');
   }
 }
